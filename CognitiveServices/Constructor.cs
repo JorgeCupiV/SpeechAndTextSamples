09ff6475484d7d30e73b3cb64a7db755399ded08
JorgeCupiV <jorgecupi@hotmail.com>
2019-06-04 11:13:29 -0500

    Added more language support to the Cogntive Services constructor class

--- CognitiveServices/Constructor.cs
+++ CognitiveServices/Constructor.cs
@@ -26,39 +26,53 @@
 
         public struct LanguageForSpeech
         {
-            public const string EnglishFemale = "en-US-ZiraRUS";
-            public const string EnglishNeuralFemale = "en-US-JessaNeural";
             public const string ChineseMainlandFemale = "zh-CN-HuihuiRUS";
             public const string ChineseMainlandNeuralFemale = "zh-CN-XiaoxiaoNeural";
+            public const string EnglishFemale = "en-US-ZiraRUS";
+            public const string EnglishNeuralFemale = "en-US-JessaNeural";
+            public const string FrenchFemale = "fr-FR-Julie-Apollo";
             public const string GermanFemale = "de-DE-Hedda";
             public const string GermanNeuralFemale = "de-DE-KatjaNeural";
+            public const string HindiFemale = "hi-IN-Kalpana";
             public const string ItalianNeuralFemale = "it-IT-ElsaNeural";
+            public const string ItalianFemale = "it-IT-LuciaRUS";
+            public const string JapaneseFemale = "ja-JP-Ayumi-Apollo";
+            public const string PortugueseBrazilFemale = "pt-BR-HeloisaRUS";
+            public const string PortuguesePortugalFemale = "pt-PT-HeliaRUS";
             public const string SpanishMexicoFemale = "es-MX-HildaRUS";
             public const string SpanishSpainFemale = "es-ES-HelenaRUS";
         }
 
         public struct Language
         {
-            public const string English = "en";
-            public const string German= "de";
-            public const string French= "fr";
-            public const string Spanish = "es";
             public const string ChineseSimplified = "zh-Hans";
             public const string ChineseTraditional = "zh-Hant";
-            public const string Hindi= "hi";
+            public const string English = "en";
+            public const string French = "fr";
+            public const string German= "de";
+            public const string Hindi = "hi";
+            public const string Italian = "it";
+            public const string Japanese = "ja";
+            public const string Portuguese = "pt";
+            public const string Spanish = "es";
         }
 
         public struct LanguageLocale
         {
-            public const string English = "en-US";
-            public const string SpanishSpain = "es-ES";
-            public const string SpanishMexico = "es-MX";
-            public const string FrenchFrance = "fr-FR";
-            public const string HindiIndia = "hi-IN";
-            public const string PortuguesePortugal = "pt-PT";
-            public const string PortugueseBrazil = "pt-BR";
             public const string ChineseMainland = "zh-CN";
             public const string ChineseHongKong = "zh-HK";
+            public const string EnglishUnitedStates = "en-US";
+            public const string EnglishUnitedKingdom = "en-UK";
+            public const string EnglishCanada = "en-CA";
+            public const string FrenchFrance = "fr-FR";
+            public const string GermanGermany = "de-DE";
+            public const string HindiIndia = "hi-IN";
+            public const string ItalianItaly = "it-IT";
+            public const string JapaneseJapan = "ja-JP";
+            public const string PortuguesePortugal = "pt-PT";
+            public const string PortugueseBrazil = "pt-BR";
+            public const string SpanishSpain = "es-ES";
+            public const string SpanishMexico = "es-MX";
         }
     }
 }
